08f085e50c439514f3a0c6d778c57537ac0f210e
Neeshka76 <83765170+Neeshka76@users.noreply.github.com>
2022-01-13 18:07:10 +0100

    Updated for U10

--- DebugMenuPlusLevelModule.cs
+++ DebugMenuPlusLevelModule.cs
@@ -11,13 +11,13 @@ namespace DebugMenuPlus
         float timeToDespawnBodies = 4.0f;
         float timer = 0.0f;
         // When a level is loaded
-        public override IEnumerator OnLoadCoroutine(Level level)
+        public override IEnumerator OnLoadCoroutine()
         {
             debugMenuPlusController = GameManager.local.gameObject.GetComponent<DebugMenuPlusController>();
-            return base.OnLoadCoroutine(level);
+            return base.OnLoadCoroutine();
         }
         // Update the location of the player
-        public override void Update(Level level)
+        public override void Update()
         {
             if (debugMenuPlusController == null)
             {
@@ -71,9 +71,9 @@ namespace DebugMenuPlus
             {
                 debugMenuPlusController.data.NbBodiesInLevelGetSet = 0;
                 // Clean Bodies
-                for (int index = Creature.list.Count - 1; index >= 0; --index)
+                for (int index = Creature.allActive.Count - 1; index >= 0; --index)
                 {
-                    if (Creature.list[index].state == Creature.State.Dead)
+                    if (Creature.allActive[index].state == Creature.State.Dead)
                     {
                         debugMenuPlusController.data.BodiesInLevelGetSet = true;
                         debugMenuPlusController.data.NbBodiesInLevelGetSet++;
@@ -84,11 +84,11 @@ namespace DebugMenuPlus
 
             IEnumerator CleanBodies()
             {
-                for (int index = Creature.list.Count - 1; index >= 0; --index)
+                for (int index = Creature.allActive.Count - 1; index >= 0; --index)
                 {
-                    if (Creature.list[index].state == Creature.State.Dead)
+                    if (Creature.allActive[index].state == Creature.State.Dead)
                     {
-                        Creature.list[index].Despawn();
+                        Creature.allActive[index].Despawn();
                     }
                     else
                     {
@@ -102,18 +102,18 @@ namespace DebugMenuPlus
             IEnumerator CountItems()
             {
                 debugMenuPlusController.data.NbItemsInLevelGetSet = 0;
-                for (int index = Item.list.Count - 1; index >= 0; --index)
+                for (int index = Item.allActive.Count - 1; index >= 0; --index)
                 {
                     //if (Item.list.Count - 1 != -1)
-                    if ((double)Item.list[index].spawnTime != 0.0
-                                && !(bool)Item.list[index].holder
-                                && (!Item.list[index].isTelekinesisGrabbed
-                                && !Item.list[index].isThrowed)
-                                && (!Item.list[index].IsHanded()
-                                && !Item.list[index].disallowDespawn
-                                && !Item.list[index].isGripped)
-                                || (Item.list[index].itemId == "ModularTile" && Item.list[index].isThrowed == true)
-                                || (Item.list[index].itemId == "DaggerCommon" && !Item.list[index].IsHanded() && !Item.list[index].isGripped && !Item.list[index].disallowDespawn && !(bool)Item.list[index].holder)
+                    if ((double)Item.allActive[index].spawnTime != 0.0
+                                && !(bool)Item.allActive[index].holder
+                                && (!Item.allActive[index].isTelekinesisGrabbed
+                                && !Item.allActive[index].isThrowed)
+                                && (!Item.allActive[index].IsHanded()
+                                && !Item.allActive[index].disallowDespawn
+                                && !Item.allActive[index].isGripped)
+                                || (Item.allActive[index].itemId == "ModularTile" && Item.allActive[index].isThrowed == true)
+                                || (Item.allActive[index].itemId == "DaggerCommon" && !Item.allActive[index].IsHanded() && !Item.allActive[index].isGripped && !Item.allActive[index].disallowDespawn && !(bool)Item.allActive[index].holder)
                                 )
                     {
                         debugMenuPlusController.data.ItemsInLevelGetSet = true;
@@ -125,22 +125,22 @@ namespace DebugMenuPlus
 
             IEnumerator CleanItems()
             {
-                for (int index = Item.list.Count - 1; index >= 0; --index)
+                for (int index = Item.allActive.Count - 1; index >= 0; --index)
                 {
                     //if (Item.list.Count - 1 != -1)
-                    if ((double)Item.list[index].spawnTime != 0.0
-                            && !(bool)Item.list[index].holder
-                            && (!Item.list[index].isTelekinesisGrabbed
-                            && !Item.list[index].isThrowed)
-                            && (!Item.list[index].IsHanded()
-                            && !Item.list[index].disallowDespawn
-                            && !Item.list[index].isGripped)
-                            || (Item.list[index].itemId == "ModularTile" && Item.list[index].isThrowed == true)
-                            || (Item.list[index].itemId == "DaggerCommon" && !Item.list[index].IsHanded() && !Item.list[index].isGripped && !Item.list[index].disallowDespawn && !(bool)Item.list[index].holder)
-                            || (Item.list[index].itemId == "ModularPotion")
+                    if ((double)Item.allActive[index].spawnTime != 0.0
+                            && !(bool)Item.allActive[index].holder
+                            && (!Item.allActive[index].isTelekinesisGrabbed
+                            && !Item.allActive[index].isThrowed)
+                            && (!Item.allActive[index].IsHanded()
+                            && !Item.allActive[index].disallowDespawn
+                            && !Item.allActive[index].isGripped)
+                            || (Item.allActive[index].itemId == "ModularTile" && Item.allActive[index].isThrowed == true)
+                            || (Item.allActive[index].itemId == "DaggerCommon" && !Item.allActive[index].IsHanded() && !Item.allActive[index].isGripped && !Item.allActive[index].disallowDespawn && !(bool)Item.allActive[index].holder)
+                            || (Item.allActive[index].itemId == "ModularPotion")
                             )
                     {
-                        Item.list[index].Despawn();
+                        Item.allActive[index].Despawn();
                     }
                     else
                     {
@@ -154,11 +154,11 @@ namespace DebugMenuPlus
             IEnumerator CountImbuements()
             {
                 debugMenuPlusController.data.NbImbuementsInLevelGetSet = 0;
-                for (int index = Item.list.Count - 1; index >= 0; --index)
+                for (int index = Item.allActive.Count - 1; index >= 0; --index)
                 {
-                    if (Item.list[index].imbues.Count != 0)
+                    if (Item.allActive[index].imbues.Count != 0)
                     {
-                        foreach (Imbue imbue in Item.list[index].imbues)
+                        foreach (Imbue imbue in Item.allActive[index].imbues)
                         {
                             if (imbue.energy != 0.0f)
                             {
@@ -173,9 +173,9 @@ namespace DebugMenuPlus
 
             IEnumerator RemoveImbuements()
             {
-                for (int index = Item.list.Count - 1; index >= 0; --index)
+                for (int index = Item.allActive.Count - 1; index >= 0; --index)
                 {
-                    foreach (Imbue imbue in Item.list[index].imbues)
+                    foreach (Imbue imbue in Item.allActive[index].imbues)
                     {
                         if (imbue.energy != 0.0f)
                         {
@@ -193,48 +193,47 @@ namespace DebugMenuPlus
 
             IEnumerator ScanItemsInfos()
             {
-                for (int index = Item.list.Count - 1; index >= 0; --index)
+                for (int index = Item.allActive.Count - 1; index >= 0; --index)
                 {
-                    if (Item.list.Count - 1 != -1)
+                    if (Item.allActive.Count - 1 != -1)
                     {
                         Debug.Log("DebugMenuPlus : ----------------------------------- : ");
                         Debug.Log("DebugMenuPlus : INDEX Number : " + index);
-                        Debug.Log("DebugMenuPlus : Item Data Id : " + Item.list[index].data.id.ToString());
-                        Debug.Log("DebugMenuPlus : Item Id : " + Item.list[index].itemId.ToString());
-                        Debug.Log("DebugMenuPlus : Item Position : " + Item.list[index].transform.position.ToString());
-                        Debug.Log("DebugMenuPlus : Item SpawnTime : " + Item.list[index].spawnTime.ToString());
-                        Debug.Log("DebugMenuPlus : Item Author : " + Item.list[index].data.author.ToString());
-                        Debug.Log("DebugMenuPlus : Item DisplayName : " + Item.list[index].data.displayName.ToString());
-                        Debug.Log("DebugMenuPlus : Item handlerArmGrabbed : " + Item.list[index].handlerArmGrabbed.ToString());
-                        //Debug.Log("DebugMenuPlus : Item holder : " + Item.list[index].holder.ToString());
-                        Debug.Log("DebugMenuPlus : Item is TK Grabbed : " + Item.list[index].isTelekinesisGrabbed.ToString());
-                        Debug.Log("DebugMenuPlus : Item is Throwned : " + Item.list[index].isThrowed.ToString());
-                        Debug.Log("DebugMenuPlus : Item is handed : " + Item.list[index].IsHanded().ToString());
-                        Debug.Log("DebugMenuPlus : Item is Disallow Despawn : " + Item.list[index].disallowDespawn.ToString());
-                        Debug.Log("DebugMenuPlus : Item is gripped : " + Item.list[index].isGripped.ToString());
-                        Debug.Log("DebugMenuPlus : Item is Flying : " + Item.list[index].isFlying.ToString());
-                        Debug.Log("DebugMenuPlus : Item is Pooled : " + Item.list[index].isPooled.ToString());
-                        Debug.Log("DebugMenuPlus : Item is Penetrating : " + Item.list[index].isPenetrating.ToString());
-                        Debug.Log("DebugMenuPlus : Item is twoHanded : " + Item.list[index].IsTwoHanded().ToString());
-                        Debug.Log("DebugMenuPlus : Item distant grab safe distance : " + Item.list[index].distantGrabSafeDistance.ToString());
-                        Debug.Log("DebugMenuPlus : Item fly rotation speed : " + Item.list[index].flyRotationSpeed.ToString());
-                        Debug.Log("DebugMenuPlus : Item fly ThrowAngle : " + Item.list[index].flyThrowAngle.ToString());
-                        //Debug.Log("DebugMenuPlus : Item Last Handler : " + Item.list[index].lastHandler.ToString());
-                        Debug.Log("DebugMenuPlus : Item Last interation time : " + Item.list[index].lastInteractionTime.ToString());
-                        Debug.Log("DebugMenuPlus : Item Name : " + Item.list[index].name.ToString());
-                        Debug.Log("DebugMenuPlus : Item Org Com : " + Item.list[index].orgCom.ToString());
-                        Debug.Log("DebugMenuPlus : Item Org Mass : " + Item.list[index].orgMass.ToString());
-                        Debug.Log("DebugMenuPlus : Item ParryPoint : " + Item.list[index].parryPoint.ToString());
-                        Debug.Log("DebugMenuPlus : Item Snap Pitch Range : " + Item.list[index].snapPitchRange.ToString());
-                        Debug.Log("DebugMenuPlus : Item use Custom Center of Mass : " + Item.list[index].useCustomCenterOfMass.ToString());
-                        Debug.Log("DebugMenuPlus : Item Data Mass : " + Item.list[index].data.mass.ToString());
-                        //Debug.Log("DebugMenuPlus : Item Data Prefab Address : " + Item.list[index].data.prefabAddress.ToString());
-                        //Debug.Log("DebugMenuPlus : Item Data Prefab Location : " + Item.list[index].data.prefabLocation.ToString());
-                        Debug.Log("DebugMenuPlus : Item Data Save Folder : " + Item.list[index].data.saveFolder.ToString());
-                        Debug.Log("DebugMenuPlus : Item Data Slot : " + Item.list[index].data.slot.ToString());
-                        Debug.Log("DebugMenuPlus : Item Data  : " + Item.list[index].GetInstanceID().ToString());
-                        //Debug.Log("DebugMenuPlus : Item Data snap Audio Container Address : " + Item.list[index].data.snapAudioContainerAddress.ToString());
-                        //Debug.Log("DebugMenuPlus : Item Data snap Audio Container Location : " + Item.list[index].data.snapAudioContainerLocation.ToString());
+                        Debug.Log("DebugMenuPlus : Item Data Id : " + Item.allActive[index].data.id.ToString());
+                        Debug.Log("DebugMenuPlus : Item Id : " + Item.allActive[index].itemId.ToString());
+                        Debug.Log("DebugMenuPlus : Item Position : " + Item.allActive[index].transform.position.ToString());
+                        Debug.Log("DebugMenuPlus : Item SpawnTime : " + Item.allActive[index].spawnTime.ToString());
+                        Debug.Log("DebugMenuPlus : Item Author : " + Item.allActive[index].data.author.ToString());
+                        Debug.Log("DebugMenuPlus : Item DisplayName : " + Item.allActive[index].data.displayName.ToString());
+                        Debug.Log("DebugMenuPlus : Item handlerArmGrabbed : " + Item.allActive[index].handlerArmGrabbed.ToString());
+                        //Debug.Log("DebugMenuPlus : Item holder : " + Item.allActive[index].holder.ToString());
+                        Debug.Log("DebugMenuPlus : Item is TK Grabbed : " + Item.allActive[index].isTelekinesisGrabbed.ToString());
+                        Debug.Log("DebugMenuPlus : Item is Throwned : " + Item.allActive[index].isThrowed.ToString());
+                        Debug.Log("DebugMenuPlus : Item is handed : " + Item.allActive[index].IsHanded().ToString());
+                        Debug.Log("DebugMenuPlus : Item is Disallow Despawn : " + Item.allActive[index].disallowDespawn.ToString());
+                        Debug.Log("DebugMenuPlus : Item is gripped : " + Item.allActive[index].isGripped.ToString());
+                        Debug.Log("DebugMenuPlus : Item is Flying : " + Item.allActive[index].isFlying.ToString());
+                        Debug.Log("DebugMenuPlus : Item is Pooled : " + Item.allActive[index].isPooled.ToString());
+                        Debug.Log("DebugMenuPlus : Item is Penetrating : " + Item.allActive[index].isPenetrating.ToString());
+                        Debug.Log("DebugMenuPlus : Item is twoHanded : " + Item.allActive[index].IsTwoHanded().ToString());
+                        Debug.Log("DebugMenuPlus : Item distant grab safe distance : " + Item.allActive[index].distantGrabSafeDistance.ToString());
+                        Debug.Log("DebugMenuPlus : Item fly rotation speed : " + Item.allActive[index].flyRotationSpeed.ToString());
+                        Debug.Log("DebugMenuPlus : Item fly ThrowAngle : " + Item.allActive[index].flyThrowAngle.ToString());
+                        //Debug.Log("DebugMenuPlus : Item Last Handler : " + Item.allActive[index].lastHandler.ToString());
+                        Debug.Log("DebugMenuPlus : Item Last interation time : " + Item.allActive[index].lastInteractionTime.ToString());
+                        Debug.Log("DebugMenuPlus : Item Name : " + Item.allActive[index].name.ToString());
+                        Debug.Log("DebugMenuPlus : Item Org Mass : " + Item.allActive[index].orgMass.ToString());
+                        Debug.Log("DebugMenuPlus : Item ParryPoint : " + Item.allActive[index].parryPoint.ToString());
+                        Debug.Log("DebugMenuPlus : Item Snap Pitch Range : " + Item.allActive[index].snapPitchRange.ToString());
+                        Debug.Log("DebugMenuPlus : Item use Custom Center of Mass : " + Item.allActive[index].useCustomCenterOfMass.ToString());
+                        Debug.Log("DebugMenuPlus : Item Data Mass : " + Item.allActive[index].data.mass.ToString());
+                        //Debug.Log("DebugMenuPlus : Item Data Prefab Address : " + Item.allActive[index].data.prefabAddress.ToString());
+                        //Debug.Log("DebugMenuPlus : Item Data Prefab Location : " + Item.allActive[index].data.prefabLocation.ToString());
+                        Debug.Log("DebugMenuPlus : Item Data Save Folder : " + Item.allActive[index].data.saveFolder.ToString());
+                        Debug.Log("DebugMenuPlus : Item Data Slot : " + Item.allActive[index].data.slot.ToString());
+                        Debug.Log("DebugMenuPlus : Item Data  : " + Item.allActive[index].GetInstanceID().ToString());
+                        //Debug.Log("DebugMenuPlus : Item Data snap Audio Container Address : " + Item.allActive[index].data.snapAudioContainerAddress.ToString());
+                        //Debug.Log("DebugMenuPlus : Item Data snap Audio Container Location : " + Item.allActive[index].data.snapAudioContainerLocation.ToString());
                     }
                 }
                 debugMenuPlusController.data.TestInfoObjectGetSet = false;
